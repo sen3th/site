ad129fa03dc8136fea2dabf745084d32a224efdf
Seneth <hi@seneth.me>
2026-01-06 16:28:43 -0500

    initial customization

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,11 @@
-export default async function(eleventyConfig) {
-	// Configure Eleventy
-};
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+}
