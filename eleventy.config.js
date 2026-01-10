caa3004f63131128b5904c705059e97a07173503
Seneth <hi@seneth.me>
2026-01-10 14:29:54 -0500

    more personalization

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,11 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css");
 
+   eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
+    const d = new Date(dateObj);
+    return d.toISOString().split("T")[0];
+  });
+  
   return {
     dir: {
       input: "src",
