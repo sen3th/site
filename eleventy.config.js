5fdd22d5e7ee8912f42d1b430766a6107223b903
Seneth <hi@seneth.me>
2026-01-17 15:32:39 -0500

    post edits, minor styling, added rss

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,13 @@
+import pluginRss from "@11ty/eleventy-plugin-rss";
+
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/favicon");
   eleventyConfig.addPassthroughCopy("src/content");
 
-   eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
+  eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
     const d = new Date(dateObj);
     return d.toISOString().split("T")[0];
   });
